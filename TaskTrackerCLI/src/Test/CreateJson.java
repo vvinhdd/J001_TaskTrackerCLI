a36161bd4a3b0ff09521eb3c44083a7bbb49f35b
Duc Vinh <ddvinh13@gmail.com>
2025-04-06 14:21:33 +0700

    Update test-function Write file JSON

---
+++ TaskTrackerCLI/src/Test/CreateJson.java
@@ -0,0 +1,40 @@
+package Test;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CreateJson {
+	
+	public static void main(String[] args) {
+		String name = "Alice";
+		int age = 25;
+		boolean isStudent = true;
+
+		String json = "{\n" +
+		              "  \"name\": \"" + name + "\",\n" +
+		              "  \"age\": " + age + ",\n" +
+		              "  \"isStudent\": " + isStudent + "\n" +
+		              "}";
+		
+		String name2 = "Alice2";
+		int age2 = 25;
+		boolean isStudent2 = true;
+		
+		String json2 = "{\n" +
+	              "  \"name\": \"" + name2 + "\",\n" +
+	              "  \"age\": " + age2 + ",\n" +
+	              "  \"isStudent\": " + isStudent2 + "\n" +
+	              "}";
+		
+
+		try( FileWriter file = new FileWriter("output.json", true) ){// true = append mode
+			file.write(json+"\n");
+			file.write(json2);
+			System.out.println("Đã ghi file Json thành công");
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		
+	}
+
+}
